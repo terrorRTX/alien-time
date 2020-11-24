663741196a72cbc1b8ed79cb7869a65c39c2e9f7
Matt <51545556+saltedsugar@users.noreply.github.com>
2020-11-24 13:25:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mins = fullDate.getMinutes();
 var secs = fullDate.getSeconds();
 var day = fullDate.getDay();
 var month = fullDate.getMonth();
-var year = fullDate.getYear();
+var year = fullDate.getFullYear();
 if (hours < 10){
     hours = "0" + hours;
 }
@@ -19,9 +19,9 @@ document.getElementById('hour').innerHTML= hours + ":";
 document.getElementById('minute').innerHTML= mins+":";
 document.getElementById('second').innerHTML= secs;
     
-document.getElementById('day').innerHTML= day + ":";
-document.getElementById('month').innerHTML= month+":";
-document.getElementById('year').innerHTML= year
+document.getElementById('day1').innerHTML= day;
+document.getElementById('month1').innerHTML= month;
+document.getElementById('year1').innerHTML= year
 
 }
 setInterval(clock,50);

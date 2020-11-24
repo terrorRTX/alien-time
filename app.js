aa078045662115218ac8880cf42bf7ac0ca171cd
Robin Timmerman <robintimmerman@hotmail.nl>
2020-11-24 20:45:16 +0100

    Alien time

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
 function clock() {
-const fullDate = new Date();
-var hours = fullDate.getHours();
-var mins = fullDate.getMinutes();
-var secs = fullDate.getSeconds();
-var day = fullDate.getDate();
-var month = fullDate.getMonth();
-var year = fullDate.getFullYear();
+	const fullDate = new Date();
+	var hours = fullDate.getHours();
+	var mins = fullDate.getMinutes();
+	var secs = fullDate.getSeconds();
+	var day = fullDate.getDate();
+	var month = fullDate.getMonth();
+	var year = fullDate.getFullYear();
 
-    
-if (hours < 10){
-    hours = "0" + hours;
-}
-if (mins < 10){
-    mins = "0" + mins;
-}
-if (secs < 10){
-    secs = "0" + secs;
-}
-if (day < 10){
-    day = "0" + day;
-}
-if (month < 10){
-    month = "0" + month;
-}
+		
+	if (hours < 10){
+		hours = "0" + hours;
+	}
+	if (mins < 10){
+		mins = "0" + mins;
+	}
+	if (secs < 10){
+		secs = "0" + secs;
+	}
+	if (day < 10){
+		day = "0" + day;
+	}
+	if (month < 10){
+		month = "0" + month;
+	}
 
-document.getElementById('hour').innerHTML= hours + ":";
-document.getElementById('minute').innerHTML= mins+":";
-document.getElementById('second').innerHTML= secs+".";
-document.getElementById('day1').innerHTML= day+"-";
-document.getElementById('month1').innerHTML= month+1+"-";
-document.getElementById('year1').innerHTML= year+"";
+	document.getElementById('hour1A').innerHTML = hours.toString()[0]; document.getElementById('hour1B').innerHTML = hours.toString()[1];
+	document.getElementById('minute1A').innerHTML = mins.toString()[0] + ":"; document.getElementById('minute1B').innerHTML = mins.toString()[1];
+	document.getElementById('second1A').innerHTML = secs.toString()[0]; document.getElementById('second1B').innerHTML = secs.toString()[1] + ":";
 
+
+	/* document.getElementById('hour').innerHTML= hours + ":";
+	document.getElementById('minute').innerHTML= mins+":";
+	document.getElementById('second').innerHTML= secs+"."; */
+	document.getElementById('day1').innerHTML= day+"-";
+	document.getElementById('month1').innerHTML= month+1+"-";
+	document.getElementById('year1').innerHTML= year+"";
 }
 setInterval(clock,50);
